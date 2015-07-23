ecfa9d69e4b54c91124ff373c4a374d1ff119f10
supershdow <benjihofing@gmail.com>
2015-07-23 10:24:22 -0400

    TAMZID IS AWFUL

--- util.py
+++ util.py
@@ -2,6 +2,12 @@ import urllib2
 import json
 
 def flickr(tag='Sloth'):
+    plus='+'
+    if tag.find(' ')!=-1:
+        tag=tag.split(' ')
+        for y in tag:
+            o=plus.join(tag)
+        tag=o
     url='https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=112e9e41ffbac9819b2f051401f0c48e&tags=%s&format=json&nojsoncallback=1'%tag
     u=urllib2.urlopen(url)
     result=u.read()
--- xcodex.py
+++ xcodex.py
@@ -3,6 +3,21 @@ import json
 
 
 def findsong(a,c):
+        plus='+'
+        if a.find(" ")!=-1:
+                a=a.split(' ')
+                for y in a:
+                        o=plus.join(a)
+                a=o
+                print a
+                print type(a)
+        if c.find(" ")!=-1:
+                c=c.split(' ')
+                for y in c:
+                    f=plus.join(c)
+                c=f
+                print c
+                print type(c)
 	s = urllib.urlopen("http://developer.echonest.com/api/v4/song/search?api_key=2ZOPM4G1WVLM5CJ17&format=json&artist=%s&title=%s&bucket=id:songmeanings"%(a,c))
 	_id = s.read()
 	z = json.loads(_id)
