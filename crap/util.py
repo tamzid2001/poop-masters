12ec97a77cd470ce4be2be79a3f63814d37a362b
supershdow <benjihofing@gmail.com>
2015-07-23 09:29:35 -0400

    Song meanings

--- crap/util.py
+++ crap/util.py
@@ -2,7 +2,7 @@ import urllib2
 import json
 
 def flickr(tag='Sloth'):
-    url='https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=44b5c3dafd75b345edf09410ed6847e6&tags=%s&format=json&nojsoncallback=1'%tag
+    url='https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=112e9e41ffbac9819b2f051401f0c48e&tags=%s&format=json&nojsoncallback=1'%tag
     u=urllib2.urlopen(url)
     result=u.read()
     w=json.loads(result)
